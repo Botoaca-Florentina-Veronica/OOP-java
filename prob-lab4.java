2a227863f56a800a396c2d7a8314bd8213086f61
Botoaca Florentina Veronica <126331242+Botoaca-Florentina-Veronica@users.noreply.github.com>
2023-10-15 14:50:12 +0300

    Update prob-lab4.java

--- prob-lab4.java
+++ prob-lab4.java
@@ -1,129 +1,132 @@
- class Remorca {
+class Remorca{
     private int nrCutii;
     private String nrInmatriculare;
 
+    //acum facem constructorii
 
-    public int getNrCutii(){
-        return this.nrCutii;
+    //primul constructor:
+    public Remorca(int nrCutii, String nrInmatriculare){
+        this.nrCutii=nrCutii;
+        this.nrInmatriculare=nrInmatriculare;
+    }
+
+    //al doilea constructor
+    public Remorca(String nrInmatriculare){
+        this.nrInmatriculare=nrInmatriculare;
+        nrCutii=10;         //initializam cu 10 fiindca nu avem alte remorci inca
+    }
+
+    public int getNrCutii() {
+        return nrCutii;
     }
 
     public String getNrInmatriculare(){
-        return this.nrInmatriculare;
-    }
-    public Remorca(int nrCutii, String nrInmatriculare) {
-        this.nrCutii = nrCutii;
-        this.nrInmatriculare = nrInmatriculare;
+        return nrInmatriculare;
     }
 
-    public Remorca(String nrInmatriculare) {
-        this.nrCutii = 10; // Valoarea implicită a cutiilor
-        this.nrInmatriculare = nrInmatriculare;
+    public String toString(){
+        return "R(" + nrInmatriculare + "," + nrCutii + ")";
+    }
+}
+
+class Tir{
+    private Remorca[] remorci;
+    private int nrRemorci;
+
+    public Tir(int maxRemorci) {
+        remorci = new Remorca[maxRemorci];
+        nrRemorci = 0; //initializam cu 0
     }
 
-    // Getteri pentru nrCutii și nrInmatriculare
+    public boolean adaugaRemorca(int nrCutii, String nrInmatriculare){
+        if(nrRemorci< remorci.length) {
+            //adica verific daca mai avem locuri in vectorul alocat remorci
+            remorci[nrRemorci] = new Remorca(nrCutii, nrInmatriculare);
+            nrRemorci++;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean adaugaRemorca(Remorca remorca){
+        if(nrRemorci<remorci.length) {
+            remorci[nrRemorci] = remorca;
+            nrRemorci++;
+            return true;
+        }
+        return false;
+    }
+
+
+    public boolean equals(Tir altTir){
+        int sumaCutiiTirCurent=0;
+        int sumaCutiiAltTir=0;
+        int i;
+
+        for (i=0; i<nrRemorci; i++) {
+            sumaCutiiTirCurent = sumaCutiiTirCurent + remorci[i].getNrCutii();
+        }
+
+        for(i=0; i<altTir.nrRemorci; i++) {
+            sumaCutiiAltTir = sumaCutiiAltTir + altTir.remorci[i].getNrCutii();
+        }
+
+        if(sumaCutiiAltTir == sumaCutiiTirCurent) {
+            return true;
+        }
+        //nu mai pun ramura de else pentru ca daca deja am returnat true, voi iesi din functie
+        //fiindca deja am un raspuns returnat
+        //in caz contrar, ies din if si returnez false
+        return false;
+    }
+
+    public Remorca stergeRemorca(String nrInmatriculare){
+        int i, j;
+        for(i=0; i<nrRemorci; i++) {
+            if(remorci[i].getNrInmatriculare().equals(nrInmatriculare)) {
+                Remorca remorcaStearsa = remorci[i];
+                for(j=i; j<nrRemorci; j++) {
+                    remorci[j]=remorci[j+1];
+                }
+                nrRemorci--;
+                return remorcaStearsa;
+            }
+        }
+        return null;
+    }
 
     @Override
-    public String toString() {
-        return "R(" + nrInmatriculare + ", " + nrCutii + ")";
+    public String toString(){
+        int i;
+        String rezultat = "T ->";
+        for(i=0; i<nrRemorci; i++) {
+            rezultat = rezultat + remorci[i].toString();
+            if(i<nrRemorci-1){
+                rezultat =  rezultat + "->";    //ca sa nu imi apara sageata si dupa ultimul element
+            }
+        }
+        return rezultat;
     }
 }
 
 
- class Tir {
-     private Remorca[] remorci;
-     private int numRemorci;
+class Main {
+    public static void main(String[] args) {
+        Tir tir1 = new Tir(5);
+        tir1.adaugaRemorca(20, "R1");
+        tir1.adaugaRemorca(15, "R2");
 
-     public Tir(int maxRemorci) {
-         remorci = new Remorca[maxRemorci];
-         numRemorci = 0;
-     }
+        Tir tir2 = new Tir(3);
+        tir2.adaugaRemorca(10, "R3");
 
-     public boolean adaugaRemorca(int nrCutii, String nrInmatriculare) {
-         if (numRemorci < remorci.length) {
-             remorci[numRemorci] = new Remorca(nrCutii, nrInmatriculare);
-             numRemorci++;
-             return true;
-         }
-         return false;
-     }
+        System.out.println("Tir1: " + tir1);
+        System.out.println("Tir2: " + tir2);
 
-     public boolean adaugaRemorca(Remorca remorca) {
-         if (numRemorci < remorci.length) {
-             remorci[numRemorci] = remorca;
-             numRemorci++;
-             return true;
-         }
-         return false;
-     }
+        System.out.println("Tir1 egal cu Tir2 ? " + tir1.equals(tir2));
 
-     public Remorca stergeRemorca(String nrInmatriculare) {
-         int i, j;
-         for (i = 0; i < numRemorci; i++) {
-             if (remorci[i].getNrInmatriculare().equals(nrInmatriculare)) {
-                 Remorca remorcaStearsa = remorci[i];
-                 //memoram in variabila remorcaStearsa valoarea de la indexul cautat pentru a-l putea ulterior returna la final
-                 // Mutam remorca din spate pentru a umple locul gol
-                 for (j = i; j < numRemorci; j++) {
-                     remorci[j] = remorci[j + 1];
-                 }
-                 numRemorci--;
-                 //remorci[numRemorci] = null; // Ștergem ultimul element(optional)
-                 return remorcaStearsa;
-             }
-         }
-         return null;
-     }
+        Remorca remorcaStearsa = tir1.stergeRemorca("R2");
+        System.out.println("Remorca stearsa: " + remorcaStearsa);
 
-     public boolean equals(Tir altTir) {
-         int sumaCutiiThis = 0;
-         int sumaCutiiOther = 0;
-         int i;
-
-         for (i = 0; i < numRemorci; i++) {
-             sumaCutiiThis = sumaCutiiThis + remorci[i].getNrCutii();
-         }
-
-         for (i = 0; i < altTir.numRemorci; i++) {
-             sumaCutiiOther = sumaCutiiOther + altTir.remorci[i].getNrCutii();
-         }
-
-         return sumaCutiiThis == sumaCutiiOther;
-     }
-
-
-     @Override
-     public String toString() {
-         int i;
-         String result = "T -> ";
-         for (i = 0; i < numRemorci; i++)
-         {
-             result = result + remorci[i].toString();
-             if (i < numRemorci - 1)
-             {
-                 result = result + " -> ";
-             }
-         }
-         return result;
-     }
- }
-
- class Main {
-     public static void main(String[] args) {
-         Tir tir1 = new Tir(5);
-         tir1.adaugaRemorca(20, "R1");
-         tir1.adaugaRemorca(15, "R2");
-
-         Tir tir2 = new Tir(3);
-         tir2.adaugaRemorca(10, "R3");
-
-         System.out.println("Tir1: " + tir1);
-         System.out.println("Tir2: " + tir2);
-
-         System.out.println("Tir1 egal cu Tir2 ? " + tir1.equals(tir2));
-
-         Remorca remorcaStearsa = tir1.stergeRemorca("R2");
-         System.out.println("Remorca stearsa: " + remorcaStearsa);
-
-         System.out.println("Tir1: " + tir1);
-     }
- }
+        System.out.println("Tir1: " + tir1);
+    }
+}
