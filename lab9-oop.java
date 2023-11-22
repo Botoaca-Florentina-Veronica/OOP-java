9c2d89b61be1db9226b729ee0625619592e80ebd
Botoaca Florentina Veronica <126331242+Botoaca-Florentina-Veronica@users.noreply.github.com>
2023-11-22 14:43:16 +0200

    Update lab9-oop.java

--- lab9-oop.java
+++ lab9-oop.java
@@ -1,36 +1,36 @@
+import java.util.*;
+
 interface Risky {
-   public double getRisk();
+    double getRisk();
 }
 
-class Programmer {
+class Member {
     private String name;
     private int age;
 
-    public Programmer(String name, int age) {
+    public Member(String name, int age) {
         this.name = name;
         this.age = age;
     }
 }
 
-class Project implements Risky {
+abstract class Project implements Risky {
     private String title;
     private String objective;
     private long funds;
-    private Programmer[] members;
-    private Programmer manager;
+    private Member[] members = new Member[15];
+    private Member manager;
     private int memberCount = 0;
-    private int maxMembers = 0;
 
-    public Project(String title, String objective, long funds, Programmer manager, int maxMembers) {
+    public Project(String title, String objective, long funds, Member manager) {
         this.title = title;
         this.objective = objective;
         this.funds = funds;
         this.manager = manager;
-        this.members = new Programmer[maxMembers];
         this.memberCount = 0;
     }
 
-    public void addMember(Programmer member) {
+    public void addMember(Member member) {
         if (memberCount < members.length) {
             members[memberCount++] = member;
         }
@@ -56,9 +56,9 @@ class CommercialProject extends Project {
     private long marketingFunds;
     private int teams;
 
-    public CommercialProject(String title, String objective, long funds, Programmer manager,
+    public CommercialProject(String title, String objective, long funds, Member manager,
                              String deadline, long marketingFunds, int teams) {
-        super(title, objective, funds, manager, 15); //trebuie sa am acelasi numar de parametrii
+        super(title, objective, funds, manager); //trebuie sa am acelasi numar de parametrii
         //ca in constructorul de la projects, pe care il extindem aici
         this.deadline = deadline;
         this.marketingFunds = marketingFunds;
@@ -75,8 +75,8 @@ class CommercialProject extends Project {
 class MilitaryProject extends Project {
     private String password;
 
-    public MilitaryProject(String title, String objective, long funds, Programmer manager, String password) {
-        super(title, objective, funds, manager, 15);
+    public MilitaryProject(String title, String objective, long funds, Member manager, String password) {
+        super(title, objective, funds, manager);
         this.password = password;
     }
 
@@ -90,15 +90,15 @@ class MilitaryProject extends Project {
 class OpenSourceProject extends Project {
     private String mailingList;
 
-    public OpenSourceProject(String title, String objective, long funds, Programmer manager, String mailingList) {
-        super(title, objective, funds, manager, 1000);
+    public OpenSourceProject(String title, String objective, long funds, Member manager, String mailingList) {
+        super(title, objective, funds, manager);
         this.mailingList = mailingList;
     }
 
     @Override
     public double getRisk() {
         // implementarea calculului de risc specific proiectului open-source
-        return (double) getMemberCount() / getFunds(); // exemplu simplu pentru moment
+        return (double) getMemberCount() / getFunds();
     }
 }
 
@@ -117,40 +117,55 @@ class InvestmentCompany {
             projects[projectCount++] = project;
         }
     }
-
     public Project getBestInvestment() {
         if (projectCount == 0) {
             return null; // Nu există proiecte
         }
 
-        Project bestInvestment = projects[0];
-        double lowestRisk = bestInvestment.getRisk();
-
-        for (int i = 1; i < projectCount; i++) {
-            double currentRisk = projects[i].getRisk();
-            if (currentRisk < lowestRisk) {
-                bestInvestment = projects[i];
-                lowestRisk = currentRisk;
+        int i, min=0;
+        for (i=1; i<projectCount; i++) {
+            if (projects[i].getRisk() < projects[min].getRisk()) {
+                projects[min] = projects[i];
             }
         }
-
-        return bestInvestment;
+        return projects[min];
     }
 
     public static void main(String[] args) {
-        InvestmentCompany company = new InvestmentCompany(3);
 
-        Programmer manager1 = new Programmer("Manager1", 30);
-        Programmer manager2 = new Programmer("Manager2", 35);
-        Programmer manager3 = new Programmer("Manager3", 40);
+        InvestmentCompany company = new InvestmentCompany(6);
 
-        Project project1 = new CommercialProject("Project1", "Objective1", 100000, manager1, "2023-12-31", 50000, 2);
-        Project project2 = new MilitaryProject("Project2", "Objective2", 150000, manager2, "password123");
-        Project project3 = new OpenSourceProject("Project3", "Objective3", 80000, manager3, "project3-list");
+        Member m1 = new Member("Vera", 30);
+        Member m2 = new Member("Ana", 35);
+        Member m3 = new Member("Sandra", 40);
+        Member p1 = new Member("Andreea", 25);
+        Member p2 = new Member("Alex", 37);
+        Member p3 = new Member("Larisa", 20);
+        Member p4 = new Member("Luca", 19);
+        Member p5 = new Member("Marius", 31);
+        Member p6 = new Member("Armina", 27);
+        Member p7 = new Member("Diana", 26);
 
-        company.addProject(project1);
-        company.addProject(project2);
-        company.addProject(project3);
+        CommercialProject c1 = new CommercialProject("Project1", "Objective1", 100000, m1, "2023-12-31", 50000, 2);
+        c1.addMember(p1);
+        c1.addMember(p2);
+        System.out.println(c1.getRisk());
+
+        MilitaryProject ml1 = new MilitaryProject("Project2", "Objective2", 150000, m2, "password123");
+        ml1.addMember(p3);
+        ml1.addMember(p4);
+        System.out.println(ml1.getRisk());
+
+        OpenSourceProject o1 = new OpenSourceProject("Project3", "Objective3", 80000, m3, "project3-list");
+        o1.addMember(p5);
+        o1.addMember(p6);
+        o1.addMember(p7);
+        System.out.println(o1.getRisk());
+
+
+        company.addProject(c1);
+        company.addProject(ml1);
+        company.addProject(o1);
 
         Project bestInvestment = company.getBestInvestment();
 
